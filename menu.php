57edfffca45ea80d152765de46c71642d4ae0d31
spider312 <github@spiderou.net>
2012-10-01 11:28:44 +0200

    Cleanup

--- menu.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Custom menu entries : Uncomment and change params, you can add multiple
-//menu_add('Menu name', 'http://menu.url', 'Menu title : a longer description for your menu entry, displayed on hover') ;
-?>
