dc261b0e9754dd3b1501f3d874ab2d56572c133b
spider312 <github@spiderou.net>
2012-10-17 17:12:14 +0200

    Line feeds in profile backup

--- download_file.php
+++ download_file.php
@@ -1,5 +1,5 @@
 <?php
 // Used by "ajax" to create a file on client's computer from a javascript content
 header('Content-Disposition: attachment; filename='.str_replace(' ', '_', $_POST['name'])) ;
-die(stripslashes($_POST['content'])) ;
+die($_POST['content']) ; // stripslashes ?
 ?>
