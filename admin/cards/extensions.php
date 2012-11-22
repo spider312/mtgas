62f35b70dc5f444eb86b7bc0e39899f8390fb821
spider312 <github@spiderou.net>
2012-11-22 11:19:12 +0100

    Display priority in extension list

--- admin/cards/extensions.php
+++ admin/cards/extensions.php
@@ -5,7 +5,7 @@ include_once '../../lib.php' ;
 include_once '../../includes/db.php' ;
 include_once '../../includes/card.php' ;
 include_once 'lib.php' ;
-
+$sort = param($_GET, 'sort', 'rd') ;
 html_head(
 	'Admin > Cards > Extensions list',
 	array(
@@ -21,6 +21,13 @@ html_menu() ;
   <div class="section">
    <h1>Extensions</h1>
    <a href="../">Return to admin</a>
+   <form method="get">
+    <select name="sort">
+     <option value="rd">Release date</option>
+     <option value="priority">Priority</option>
+    </select>
+    <input type="submit">
+   </form>
 <?php
 $ext = param($_GET, 'ext_del', 0) ;
 if ( $ext != 0 ) {
@@ -34,10 +41,11 @@ if ( $ext != 0 ) {
      <th>Full name</th>
      <th>Cards in DB</th>
      <th>Release date</th>
+     <th title="For images, higher priority is selected">Priority</th>
      <th>Actions</th>
     </tr>
 <?php
-$query = query('SELECT *, UNIX_TIMESTAMP(release_date) as rd FROM extension ORDER BY release_date ASC') ;
+$query = query('SELECT *, UNIX_TIMESTAMP(release_date) as rd FROM extension ORDER BY '.$sort.' ASC') ;
 while ( $arr = mysql_fetch_array($query) ) {
 	$nbcards = 0 ;
 	$query_b = query('SELECT * FROM card_ext WHERE `ext` = '.$arr['id']) ;
@@ -48,6 +56,7 @@ while ( $arr = mysql_fetch_array($query) ) {
 	echo '     <td>'.$arr['name'].'</td>'."\n" ;
 	echo '     <td>'.$nbcards.'</td>'."\n" ;
 	echo '     <td>'.date('d F Y', $arr['rd']).'</td>'."\n" ;
+	echo '     <td>'.$arr['priority'].'</td>' ;
 	echo '     <td><a href="?ext_del='.$arr['id'].'">del</a></td>'."\n" ;
 	echo '    </tr>'."\n" ;
 }
