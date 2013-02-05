b42fd8dbd20e87de7520a2c23452043d084aa22b
spider312 <github@spiderou.net>
2013-02-05 14:47:19 +0100

    Another forgotten file

---
+++ admin/tournament/json.php
@@ -0,0 +1,24 @@
+<?php
+if ( array_key_exists('id', $_GET) ) {
+	include '../../lib.php' ;
+	include '../../includes/db.php' ;
+	$id = param_or_die($_GET, 'id') ;
+	$status = param_or_die($_GET, 'status') ;
+	$due_time = param_or_die($_GET, 'due_time') ;
+	$updates = array() ;
+	if ( $status != '' )
+		$updates[] = "`status` = '".mysql_real_escape_string($status)."'" ;
+	if ( $due_time != '' )
+		$updates[] = "`due_time` = '".mysql_real_escape_string($due_time)."'" ;
+	$q = "UPDATE 
+		`tournament`
+	SET
+		".implode(', ', $updates)."
+	WHERE
+		`id` = '$id'
+	; " ;
+	$query = query($q, 'Tournament update') ;
+	die('{"nb": '.mysql_affected_rows().'}') ;
+} else
+	die('{"msg" : "No ID in param"}') ;
+?>
