a5d371526a6c47a18fb891f45d878689d203aae2
spider312 <github@spiderou.net>
2013-05-04 18:52:48 +0200

    Decks translation

--- index_decks.php
+++ index_decks.php
@@ -1,45 +1,48 @@
    <div id="decks" class="section"><!-- ---------- Decks ---------- -->
-    <h1>Decks</h1>
+    <h1><?=__('index.decks');?></h1>
     <!-- Deck list header -->
     <table id="decks_table">
      <thead>
       <tr>
-       <th class="first_col leftalign">Name</th>
-       <th colspan="2" class="rightalign nowrap">Actions : <button id="deck_edit" title="View and change selected deck's list">Edit</button><button id="deck_delete" title="Remove selected deck from list">Delete</button><button id="deck_export" title="Save selected deck as a .mwdeck file">Export</button>
+       <th class="first_col leftalign"><?=__('index.decks.name');?></th>
+       <th colspan="2" class="rightalign nowrap"><?=__('index.decks.actions');?>
+        <button id="deck_edit" title="<?=__('index.decks.actions.edit.title');?>"><?=__('index.decks.actions.edit');?></button>
+        <button id="deck_delete" title="<?=__('index.decks.actions.delete.title');?>"><?=__('index.decks.actions.delete');?></button>
+        <button id="deck_export" title="<?=__('index.decks.actions.export.title');?>"><?=__('index.decks.actions.export');?></button>
        </th>
       </tr>
      </thead>
      <!-- Deck list -->
      <tbody id="decks_list">
-      <tr><td colspan="6">Waiting for preloaded decks to load</td></tr>
+      <tr><td colspan="6"><?=__('index.decks.waiting');?></td></tr>
      </tbody>
      <tfoot>
       <!-- Create -->
-      <tr title="A deck from scratch">
+      <tr title="<?=__('index.decks.create.title');?>">
        <form id="deck_create" method="get" action="deckbuilder.php">
-        <th class="nowrap">From scratch</th>
-        <td><input type="text" name="deck" placeholder="Deck name" title="Name of the deck"></td>
-        <th class="last_col"><input type="submit" value="Create" class="fullwidth"></th>
+        <th class="nowrap"><?=__('index.decks.create');?></th>
+        <td><input type="text" name="deck" placeholder="<?=__('index.decks.create.name');?>" title="<?=__('index.decks.create.name.title');?>"></td>
+        <th class="last_col"><input type="submit" value="<?=__('index.decks.create.create');?>" class="fullwidth"></th>
        </tr>
       </form>
-      <!-- Load -->
-      <tr title="Deck files on your computer">
-       <th class="nowrap">From your computer</th>
+      <!-- Upl -->
+      <tr title="<?=__('index.decks.upload.title');?>">
+       <th class="nowrap"><?=__('index.decks.upload');?></th>
        <td>
         <form id="upload">
-         <input type="file" multiple id="deckfile" name="deckfile" class="fullwidth" accesskey="u" title="Deck files (in MWS (.mwDeck) or Aprentice (.dec) file format). You can select multiple with Ctrl, Shift or mouse selection">
+         <input type="file" multiple id="deckfile" name="deckfile" class="fullwidth" accesskey="u" title="<?=__('index.decks.upload.file.title');?>">
       </form>
        </td>
-       <th><input type="submit" value="Import" class="fullwidth"></th>
+       <th><input type="submit" value="<?=__('index.decks.upload.file.upload');?>" class="fullwidth"></th>
       </tr>
       <!-- Download -->
       <form id="download" action="" method="get">
-       <tr title="A deck file hosted by a web-server, paste the 'export MWS' link on a deck on mtgtop8.com for example">
-        <th class="nowrap">From a website</th>
+       <tr title="<?=__('index.decks.download.title');?>">
+        <th class="nowrap"><?=__('index.decks.download');?></th>
         <td>
-          <input id="deck_url" type="text" name="deck_url" placeholder="Deck URL" title="URL of the deck file (in MWS (.mwDeck) or Aprentice (.dec) file format)">
+          <input id="deck_url" type="text" name="deck_url" placeholder="<?=__('index.decks.download.placeholder');?>" title="<?=__('index.decks.download.url.title');?>">
         </td>
-        <th><input type="submit" value="Download" class="fullwidth"></th>
+        <th><input type="submit" value="<?=__('index.decks.download.download');?>" class="fullwidth"></th>
        </tr>
       </form>
      </tfoot>
