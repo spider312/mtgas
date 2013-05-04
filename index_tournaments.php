1e07f03aa5712020da08c85e65f4e1ebe398c5df
spider312 <github@spiderou.net>
2013-05-04 16:14:29 +0200

    French translation + index tournament

--- index_tournaments.php
+++ index_tournaments.php
@@ -1,105 +1,105 @@
    <div id="tournaments" class="section"><!-- ---------- tournaments ---------- -->
-    <h1>Tournaments</h1>
+    <h1><?=__('index.tournaments');?></h1>
     <div id="tournament_join" class="hidden">
-     <h2>Join</h2>
+     <h2><?=__('index.tournaments.join');?></h2>
      <table id="tournament_list">
       <thead>
        <tr>
-        <td>ID</td>
-        <td>Type</td>
-        <td>Game name</td>
-        <td>Age</td>
-        <td>Slots</td>
-        <td>Players</td>
-        <td>View</td>
+        <td><?=__('index.tournaments.id');?></td>
+        <td><?=__('index.tournaments.type');?></td>
+        <td><?=__('index.tournaments.name');?></td>
+        <td><?=__('index.tournaments.age');?></td>
+        <td><?=__('index.tournaments.slots');?></td>
+        <td><?=__('index.tournaments.players');?></td>
+        <td><?=__('index.tournaments.view');?></td>
        </tr>
       </thead>
       <tbody id="tournament_no" style="display: none ;">
        <tr>
-        <td colspan="7">No pending tournaments</td>
+        <td colspan="7"><?=__('index.tournaments.join.no');?></td>
        </tr>
       </tbody>
       <tbody id="pending_tournaments">
        <tr>
-        <td colspan="7">Waiting for list of pending tournaments</td>
+        <td colspan="7"><?=__('index.tournaments.join.wating');?></td>
        </tr>
      </tbody>
      </table>
     </div><!-- id="tournament_join" -->
-    <h2>Create</h2>
+    <h2><?=__('index.tournaments.create');?></h2>
     <form id="tournament_create" action="tournament/json/create.php" method="post">
-     <label title="Tournament's format">Format : 
+     <label title="<?=__('index.tournaments.create.format.title');?>"><?=__('index.tournaments.create.format');?>
       <select id="tournament_type" name="type">
-      <optgroup label="Limited">
-        <option value="draft">Draft</option>
-        <option value="sealed">Sealed</option>
+      <optgroup label="<?=__('index.tournaments.create.limited');?>">
+        <option value="draft"><?=__('index.tournaments.create.draft');?></option>
+        <option value="sealed"><?=__('index.tournaments.create.sealed');?></option>
        </optgroup>
-       <optgroup label="Constructed">
-        <option value="vintage">Vintage (T1)</option>
-        <option value="legacy">Legacy (T1.5)</option>
-        <option value="extended">Extended (T1.X)</option>
-        <option value="standard">Standard (T2)</option>
-        <option value="edh">EDH</option>
+       <optgroup label="<?=__('index.tournaments.create.constructed');?>">
+        <option value="vintage"><?=__('index.tournaments.create.vintage');?></option>
+        <option value="legacy"><?=__('index.tournaments.create.legacy');?></option>
+        <option value="extended"><?=__('index.tournaments.create.modern');?></option>
+        <option value="standard"><?=__('index.tournaments.create.standard');?></option>
+        <option value="edh"><?=__('index.tournaments.create.edh');?></option>
        </optgroup>
       </select>
      </label>
-     <label title="Tournament's name">Name : 
-      <input type="text" id="tournament_name" name="name" placeholder="Tournament's name" size="64">
+     <label title="<?=__('index.tournaments.create.name.title');?>"><?=__('index.tournaments.create.name');?>
+      <input type="text" id="tournament_name" name="name" placeholder="<?=__('index.tournaments.create.name_placeholder');?>" size="64">
      </label>
-     <label title="Number of players">Players : 
+     <label title="<?=__('index.tournaments.create.players.title');?>"><?=__('index.tournaments.create.players');?>
       <input type="text" id="tournament_players" name="players" value="2" size="2" maxlength="2">
      </label>
      <div id="limited">
-      <label id="tournament_suggestions_label" title="Suggestions for tournament's boosters">Boosters suggestions : 
+      <label id="tournament_suggestions_label" title="<?=__('index.tournaments.create.suggestions.title');?>"><?=__('index.tournaments.create.suggestions');?>
        <select id="tournament_suggestions"></select>
       </label>
-      <label id="tournament_boosters_label" title="Tournament's boosters">Boosters for tournament : 
+      <label id="tournament_boosters_label" title="<?=__('index.tournaments.create.boosters.title');?>"><?=__('index.tournaments.create.boosters');?>
        <input type="text" id="tournament_boosters" name="boosters" value="" maxlength="128">
-       <input type="button" id="boosters_reset" value="Reset">
+       <input type="button" id="boosters_reset" value="<?=__('index.tournaments.create.reset');?>">
       </label>
-      <label id="booster_suggestions_label" title="Add one booster selected in list">Custom booster : 
+      <label id="booster_suggestions_label" title="<?=__('index.tournaments.create.booster_add.title');?>"><?=__('index.tournaments.create.booster_add');?>
        <select id="booster_suggestions">
-        <option disabled="disabled">Waiting for list</option>
+        <option disabled="disabled"><?=__('index.tournaments.create.waiting');?></option>
        </select>
-       <input id="booster_add" type="button" value="Add">
+       <input id="booster_add" type="button" value="<?=__('index.tournaments.create.add');?>">
       </label>
      </div><!-- id="limited" -->
-     <fieldset id="tournament_options" class="shrinked" title="Click the + to get more options">
-      <legend><input type="button" id="tournament_options_toggle" value="+">Options</legend>
-      <label title="Force more rounds even if not enough players">Number of rounds : 
+     <fieldset id="tournament_options" class="shrinked" title="<?=__('index.tournaments.create.options.title');?>">
+      <legend><input type="button" id="tournament_options_toggle" value="+"><?=__('index.tournaments.create.options');?></legend>
+      <label title="<?=__('index.tournaments.create.round_number.title');?>"><?=__('index.tournaments.create.round_number');?>
        <input type="text" name="rounds_number" value="0">
       </label>
-      <label title="Change duration of rounds, in minutes">Rounds duration : 
+      <label title="<?=__('index.tournaments.create.round_duration.title');?>"><?=__('index.tournaments.create.round_duration');?>
        <input type="text" name="rounds_duration" value="<?php echo round($round_duration/60) ?>">
       </label>
-      <label title="All players in the sealed will have the same pool">Clone sealed : 
+      <label title="<?=__('index.tournaments.create.clone.title');?>"><?=__('index.tournaments.create.clone');?>
        <input type="checkbox" name="clone_sealed" value="true">
       </label>
      </fieldset>
      <input type="hidden" id="draft_boosters" name="draft_boosters" value="">
      <input type="hidden" id="sealed_boosters" name="sealed_boosters" value="">
-     <input class="create" type="submit" value="Create">
+     <input class="create" type="submit" value="<?=__('index.tournaments.create.create');?>">
     </form>
     <div id="tournament_view" class="hidden">
-     <h2>View</h2>
+     <h2><?=__('index.tournaments.view');?></h2>
      <table id="running_tournament_list">
       <thead>
        <tr>
-        <td>Type</td>
-        <td>Name</td>
-        <td>Status</td>
-        <td title="Before end of current phase, or round">Time left</td>
-        <td>Players</td>
+        <td><?=__('index.tournaments.type');?></td>
+        <td><?=__('index.tournaments.name');?></td>
+        <td><?=__('index.tournaments.status');?></td>
+        <td title="><?=__('index.tournaments.time_left.title');?>"><?=__('index.tournaments.time_left');?></td>
+        <td><?=__('index.tournaments.players');?></td>
        </tr>
       </thead>
       <tbody id="running_tournament_no" style="display: none ;">
        <tr>
-        <td colspan="6">No running tournaments</td>
+        <td colspan="6"><?=__('index.tournaments.view.no');?></td>
        </tr>
       </tbody>
       <tbody id="running_tournaments">
        <tr>
-        <td colspan="6">Waiting for list of running tournaments</td>
+        <td colspan="6"><?=__('index.tournaments.view.waiting');?></td>
        </tr>
       </tbody>
      </table>
