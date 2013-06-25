d68e1c6cb92bb78bb1d2e2d393923ec8ce999c36
spider312 <github@spiderou.net>
2013-06-25 19:04:32 +0200

    Double face cards parsing

--- admin/cards/import/importer/mv.php
+++ admin/cards/import/importer/mv.php
@@ -3,6 +3,14 @@
 $list_url = 'http://www.magic-ville.com/fr/set_cards.php?setcode='.$ext_source.'&lang=eng' ;
 $list_regex = '#<img src="graph/rarity/(?<rarity>.*?)\.gif"><a (class=und)? href=(?<url>carte\.php\?ref=.*?)>(?<name>.*?)</a>#' ;
 
+// Lib
+function convert_html_to_text($tmp) {
+	$tmp = preg_replace('#<img style="vertical-align:-20%;" src=graph/manas_c/(.)(.).gif alt="%\1\2">#', '{$1/$2}', $tmp) ; // Hybrid
+	$tmp = preg_replace('#<img style="vertical-align:-20%;" src=graph/manas_c/(.).gif alt="%\1">#', '{$1}', $tmp) ; // Normal
+	$tmp = str_replace(' : ', ': ', $tmp) ; // Stick to MCI policy
+	return $tmp ;
+}
+
 // Get
 $html = cache_get($list_url, 'cache/'.$source.'_'.$ext_source, $verbose) ;
 $nb = preg_match_all($list_regex, $html, $matches_list, PREG_SET_ORDER) ;
@@ -30,7 +38,7 @@ foreach ( $matches_list as $match ) { //
 				$cost .= '{'.implode('/', str_split($match_cost['mana'])).'}' ;
 	// Name, type and text
 	$nb = preg_match_all('#<div style=".*></div>
-\s*<div class=S16>(?<name>.*?)</div>
+\s*<div class=S16>(<img src=graph/moteur/sun.png> )?(?<name>.*?)</div>
 \s*<div class=G12 style="padding-top:4px;padding-bottom:.px;">(?<type>.*)</div>
 \s*<div align=center>
 \s*<div id=EngShort style="display:block;" class=S1. align=justify>(?<text>.*?)</div>#s', $html, $matches, PREG_SET_ORDER) ;
@@ -38,13 +46,15 @@ foreach ( $matches_list as $match ) { //
 		echo '<a href="'.$url.'" target="_blank">regex failed</a> -> '.$path ;
 		return false ;
 	}
+
 	// Text
 		// Type specific
 	$text = '' ;
 	$pow = '' ;
 	$tou = '' ;
 			// Creature
-	if ( preg_match_all('#<div align=right class=G14 style="padding-right:4px;">(?<pt>(?<pow>[^\s]*)/(?<tou>[^\s]*))</div>#', $html, $matches_pt, PREG_SET_ORDER) > 0 ) {
+	$nb2 = preg_match_all('#<div align=right class=G14 style="padding-right:4px;">(?<pt>(?<pow>[^\s]*)/(?<tou>[^\s]*))</div>#', $html, $matches_pt, PREG_SET_ORDER) ;
+	if ( $nb2 > 0 ) {
 		$pt = $matches_pt[0]['pt'] ;
 		$pow = $matches_pt[0]['pow'] ;
 		$tou = $matches_pt[0]['tou'] ;
@@ -54,12 +64,8 @@ foreach ( $matches_list as $match ) { //
 		$text = $matches_loyalty[0]['loyalty']."\n" ;
 		$matches[0]['text'] = preg_replace('@\s*<tr .*?'.'>\s*<td valign=middle width=36><table width=30 bgcolor=#000000 cellpadding=2 cellspacing=0 align=center><tr><td class=W12 align=center>\s*(.*?)</td></tr></table></td>\s*<td class=S11 align=justify>(.*?)</td>\s*</tr>@', '$1: $2'."\n", $matches[0]['text']) ; // Planeswalkers steps
 	}
-	$tmp = $matches[0]['text'] ;
-	// Cost parsing before strip_tags
-	$tmp = preg_replace('#<img style="vertical-align:-20%;" src=graph/manas_c/(.)(.).gif alt="%\1\2">#', '{$1/$2}', $tmp) ; // Hybrid
-	$tmp = preg_replace('#<img style="vertical-align:-20%;" src=graph/manas_c/(.).gif alt="%\1">#', '{$1}', $tmp) ; // Normal
-	$tmp = str_replace(' : ', ': ', $tmp) ;
-	$text .= trim(strip_tags($tmp)) ;
+	// Costs parsing before strip_tags
+	$text .= trim(strip_tags(convert_html_to_text($matches[0]['text']))) ;
 	// Name
 	$name = card_name_sanitize($matches[0]['name']) ;
 	$type = trim($matches[0]['type']) ;
@@ -77,19 +83,37 @@ foreach ( $matches_list as $match ) { //
 		}
 		$text .= "\n----\n$altcost\n$type\n".trim(strip_tags($matches[1]['text'])) ;
 	}
+	// Face down
+	//echo $matches[0]['name'].' : '.$nb.', '.$nb2.'<br>' ;
+	$nbmoon = preg_match_all('#<div class=S16><img src=graph/moteur/moon.png> (?<name>.*?)</div><div class=G12 style="padding-top:4px;padding-bottom:3px;">(?<colors><img .*>) (?<type>.*)</div><div style="display:block;" class=S12 align=justify>(?<text>.*?)</div>#', $html, $matches_moon, PREG_SET_ORDER) ;
+	if ( $nbmoon > 0 ) {
+		$match_moon = $matches_moon[1] ; // 0 is french version
+		$nbcolor = preg_match_all('#<img  height=10 src=graph/manas/l(?<color>.).gif>#', $match_moon['colors'], $matches_colors, PREG_SET_ORDER) ;
+		$ci = '%' ;
+		if ( $nbcolor > 0 )
+			foreach ( $matches_colors as $color )
+				$ci .=  $color['color'] ;
+		else
+			echo 'Color not found<br>' ;
+		$text .= "\n-----\n".card_name_sanitize($match_moon['name'])."\n$ci ".$match_moon['type']."\n" ;
+		if ( count($matches_pt) > 1 ) // Multiple P/T found, first is day, other are night
+			$text .= $matches_pt[1]['pow'].'/'.$matches_pt[1]['tou']."\n" ;
+		$text .= trim(strip_tags(convert_html_to_text($match_moon['text']))) ;
+	}
+
 	$token = false ;
 	// Rarity
 	$rarity = '' ;
 	if ( strpos($name, 'Guildgate') != false )
 		$rarity = 'L' ;
 	else {
-		$rarities = array(4 => 'M', 10 => 'R', 20 => 'U', 30 => 'C') ;
+		$rarities = array(4 => 'M', 10 => 'R', 20 => 'U', 30 => 'C', 40 => 'L') ;
 		if ( preg_match_all('#<img src=graph/rarity/carte(\d{1,2}).gif( border=0)?'.'>#', $html, $matches_rarity, PREG_SET_ORDER) > 0 ) {
 			$rid = intval($matches_rarity[0][1]) ;
 			if ( isset($rarities[$rid]) )
 				$rarity = $rarities[$rid] ;
 			else {
-				echo '?'.$rid.'?' ;
+				echo 'Unknown rarity ID : "'.$rid.'"' ;
 				$token = true ;
 			}
 		} else
