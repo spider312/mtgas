9a64117ebf1aa6a8fe263332a17e985f4e64fc1c
spider312 <github@spiderou.net>
2013-10-06 18:58:28 +0200

    Parse games finding relationship between starter and winner

--- admin/tournament/sealed_parse.php
+++ admin/tournament/sealed_parse.php
@@ -11,17 +11,14 @@ ini_set ('memory_limit', '256M');
 $card_connection = card_connect() ;
 // All decks from all sealed events
 $q = "SELECT
-	`registration`.`player_id`,
-	`registration`.`deck`,
 	`tournament`.`id`,
 	`tournament`.`name`,
 	`tournament`.`creation_date`,
 	`tournament`.`data`
 FROM
-	`registration`, `tournament`
+	`tournament`
 WHERE
-	`registration`.`tournament_id` = `tournament`.`id`
-	AND `tournament`.`min_players` > 1
+	`tournament`.`min_players` > 1
 	AND  (
 		`tournament`.`type` = 'sealed'
 		OR `tournament`.`type` = 'draft'
@@ -32,31 +29,82 @@ if ( $name != '' )
 	$q .= "	AND `tournament`.`name` LIKE '%$name%'
 " ;
 $q .= " ;" ;
-$r = query_as_array($q) ;
-echo 'Query : '.count($r)."\n" ;
+$t = query_as_array($q) ;
+echo 'Query : '.count($t)."\n" ;
 // Parse each
 $start = microtime(true) ;
 $cards = array() ;
-$i = 0 ;
+$parsed = 0 ;
 echo '<pre>' ;
-foreach ( $r as $d ) {
+$starter_won = $starter_lost = 0 ;
+foreach ( $t as $d ) {
+	echo $d->creation_date.' - '.'<a href="/tournament/?id='.$d->id.'">'.$d->name.'</a> : ' ;
 	$data = json_decode($d->data) ;
-	if ( property_exists($data, 'score') && property_exists($data->score, $d->player_id) && property_exists($data->score->{$d->player_id}, 'gamepoints') ) {
-		$deck = deck2arr($d->deck, true) ;
+	if ( ! property_exists($data, 'score') ) {
+		echo "Unparsed : no score\n" ;
+		continue ;
+	}
+	foreach ( $data->results as $i => $round ) {
+		//echo "\n\tParsing round #".$i ;
+		foreach ( $round as $game ) {
+			//echo "\n\t\tParsing game #".$game->id ;
+			$a = query_as_array("SELECT * FROM `action` WHERE `action`.`game` = '".$game->id."'") ;
+			$starter = '' ;
+			foreach ( $a as $action )
+				switch ( $action->type ) {
+					case 'choose' :
+						$param = json_decode($action->param) ;
+						$starter = $param->player ;
+						if ( $starter == 'game.creator' )
+							$starter = $game->creator_id ;
+						elseif ( $starter == 'game.joiner' )
+							$starter = $game->joiner_id ;
+						break ;
+					case 'psync' :
+						$param = json_decode($action->param) ;
+						if ( isset($param->attrs->score) ) {
+							if ( $starter == '' ) {
+								$starter_lost++ ;
+								//die("\n\t\t\tLoser while no starter : ".$action->sender) ;
+							} else {
+								if ( $starter == $action->sender )
+									//echo "\n\t\t\tStarter lost" ;
+									$starter_lost++ ;
+								else
+									//echo "\n\t\t\tStarter won" ;
+									$starter_won++ ;
+							}
+						}
+						break ;
+				}
+		}
+	}
+	$g = query_as_array("SELECT * FROM `registration` WHERE `registration`.`tournament_id` = '".$d->id."'") ;
+	foreach ( $g as $r ) {
+		if ( ! property_exists($data->score, $r->player_id) ) {
+			echo "Unparsed : no score for ".$g->nick."\n" ;
+			continue ;
+		}
+		if ( ! property_exists($data->score->{$r->player_id}, 'gamepoints') ) {
+			echo "Unparsed : no gamepoints for ".$r->nick."\n" ;
+			continue ;
+		}
+		$deck = deck2arr($r->deck, true) ;
 		$rounds_number = 1 ;
 		if ( $data->rounds_number > $rounds_number )
 			$rounds_number = $data->rounds_number ;
-		$score = $data->score->{$d->player_id}->gamepoints/(6*$rounds_number) ; // 3 gamepoints per game win : 6 gamepoint = win, 3 gamepoints = lose 2-1, 0 gamepoints = lose 2-0
+		$score = $data->score->{$r->player_id}->gamepoints/(6*$rounds_number) ; // 3 gamepoints per game win : 6 gamepoint = win, 3 gamepoints = lose 2-1, 0 gamepoints = lose 2-0
 		foreach ( $deck->main as $id => $card ) // Played cards
 			update_score($card->id, 1, $score) ;
 		foreach ( $deck->side as $id => $card ) // Not played cards
 			update_score($card->id) ;
-		$i++ ;
-		echo $i." : ".$d->creation_date." - ".$d->name."\n" ;
-	} else
-		echo 'Unparsed : '.$d->id."\n" ;
+		$parsed++ ;
+	}
+	//echo "Parsed #$parsed\n" ;
+	echo "\n" ;
 }
-echo "\n</pre>".'Parse ('.$i.') : '.(microtime(true)-$start).'s'."\n" ;
+echo "\n</pre>".'Parse ('.$parsed.') : '.(microtime(true)-$start).'s'."\n" ;
+echo "<br>Starter won $starter_won , Starter lost $starter_lost\n" ;
 // Table will be filled, start by empty it
 query('TRUNCATE TABLE `mtg`.`pick` ; ', 'truncate', $card_connection) ;
 // Insert
