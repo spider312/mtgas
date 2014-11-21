2d3c81ca07e2aad282881fe0639b16bf49853def
spider312 <github@spiderou.net>
2014-11-21 13:55:43 +0100

    Split cards name parsing

--- includes/card.php
+++ includes/card.php
@@ -112,7 +112,7 @@ function card_name_sanitize($name) {
 	//$name = str_replace("'", '‘', $name) ;
 	$name = str_replace(array('“', '”'), '"', $name) ;
 	// Lotus Noir
-	$name = str_replace(' // ', ' / ', $name) ; // Idem with escaping
+	$name = preg_replace('/\s*\/+\s*/', ' / ', $name) ; // corrects "a // b", "a/b"
 	return $name ;
 }
 function card_text_sanitize($text) {
