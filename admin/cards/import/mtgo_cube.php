02b541c1235a8851f858ec043154090b4e905e5a
spider312 <github@spiderou.net>
2014-11-24 17:28:58 +0100

    Updated for new Wizards website

--- admin/cards/import/mtgo_cube.php
+++ admin/cards/import/mtgo_cube.php
@@ -1,11 +1,16 @@
 <?php
+// http://magic.wizards.com/en/MTGO/articles/archive/magic-online-legacy-cube-card-list
 include_once 'lib.php' ;
 $url = param_or_die($_GET, 'url') ;
 $html = cache_get($url, 'cache/mtgo_cube', false) ;
-$regex = '@<td><a class="nodec" keyName="name" keyValue="(?<key>.*?)" onmouseover="OpenTip\(event, this\)" onclick="autoCardWindow\(this\)" href="javascript:void\(\)">(?<name>.*?)</a>(?<bonus>.*?)</td>@' ;
+$regex = '@<a href=".*?" class="autocard-link" data-image-url=".*?">(?<name>.*?)</a>@' ;
 if ( preg_match_all($regex, $html, $cards, PREG_SET_ORDER) < 1 )
 	die('No card found') ;
 header('Content-Disposition: attachment; filename=OMC.txt') ;
-foreach ( $cards as $card )
-	echo str_replace('//', ' / ', $card['name'])."\n" ;
+foreach ( $cards as $card ) {
+	$name = $card['name'] ;
+	$name = html_entity_decode($name) ; // Far &amp; Away
+	$name = str_replace('&',  '/', $name) ;
+	echo $name."\n" ;
+}
 ?>
