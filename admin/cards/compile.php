198599542dfb6c33e3b4e1bb6d2fe8a629335481
spider312 <github@spiderou.net>
2014-12-23 22:33:12 +0100

    Add a little bit of reporting at the end of compile

--- admin/cards/compile.php
+++ admin/cards/compile.php
@@ -1,10 +1,12 @@
 <pre><?php
 include_once 'lib.php' ;
+$nb = 0 ;
 $query = query('SELECT * FROM card ORDER BY `id` DESC') ; // Last added cards are managed first, in order not to wait before a die
 while ( $arr = mysql_fetch_array($query) ) {
 	$arr['text'] = card_text_sanitize($arr['text']) ;
 	$attrs = json_encode(new attrs($arr)) ;
 	if ( $arr['attrs'] != $attrs ) {
+		$nb++ ;
 		echo '<hr>'.$arr['name'] ;
 		echo '<pre>-'.print_r(obj_diff(json_decode($arr['attrs']), json_decode($attrs)), true).'</pre>';
 		echo '<pre>+'.print_r(obj_diff(json_decode($attrs), json_decode($arr['attrs'])), true).'</pre>';
@@ -18,5 +20,5 @@ while ( $arr = mysql_fetch_array($query) ) {
 		`id` = '".$arr['id']."'
 	; ") ;
 }
-die('end') ;
+die($nb.' updates') ;
 ?>
