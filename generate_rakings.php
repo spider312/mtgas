2914f58e5c06ce73a534e8e20f7eb9937f7d2291
spider312 <github@spiderou.net>
2015-02-15 00:09:41 +0100

    Generate rankings without rebooting server

---
+++ generate_rakings.php
@@ -0,0 +1,11 @@
+#!/bin/php
+<?php
+
+include 'lib.php' ;
+include 'includes/db.php' ;
+include 'includes/ranking.php' ;
+
+ranking_to_file('ranking/week.json', 'WEEK') ;
+ranking_to_file('ranking/month.json', 'MONTH') ;
+ranking_to_file('ranking/year.json', 'YEAR') ;
+ranking_to_file('ranking/year.json', 'YEAR', 10) ;
