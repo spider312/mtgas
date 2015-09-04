9f74802fb672445398ad1b17ac12295a5d72a207
spider312 <github@spiderou.net>
2015-09-04 20:27:07 +0200

    Better debug message for the duel cleaning bug tracking

--- includes/classes/ws_gameserver.php
+++ includes/classes/ws_gameserver.php
@@ -238,11 +238,11 @@ class GameServer {
 	public function clean_duel($duel) {
 		$i = array_search($duel, $this->joined_duels) ;
 		if ( $i > -1 ) {
-			$this->say('Delete duel '.$duel->name) ;
 			$duels = array_splice($this->joined_duels, $i, 1) ;
 			foreach ( $duels as $spliceduel )
 				unset($spliceduel) ;
-		}
+		} else
+			$this->say('Joined duel not found : '.$duel->name) ;
 	}
 		// Tournament
 	public function move_tournament($tournament, $from, $to) {
