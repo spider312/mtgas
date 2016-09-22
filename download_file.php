6504d1aacdf7e65ff35bb4f5570568a27844fb84
spider312 <github@spiderou.net>
2016-09-22 09:56:44 +0200

    Make browser interpret deck file as plain text fil instead of HTML (display newlines)

--- download_file.php
+++ download_file.php
@@ -1,5 +1,6 @@
 <?php
 // Used by "ajax" to create a file on client's computer from a javascript content
 header('Content-Disposition: attachment; filename='.str_replace(' ', '_', $_POST['name'])) ;
+header('Content-type: text/plain');
 die($_POST['content']) ; // stripslashes ?
 ?>
