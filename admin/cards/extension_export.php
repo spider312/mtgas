4c513fe62491a913e2a7fb8c4cf80372a2f7e95b
spider312 <github@spiderou.net>
2017-04-15 21:02:37 +0200

    Add text in extension CSV export

--- admin/cards/extension_export.php
+++ admin/cards/extension_export.php
@@ -6,7 +6,7 @@ if ( $e = mysql_fetch_object($equery) ) {
 	header("Content-type: application/csv-tab-delimited-table"); 
 	header("Content-disposition: attachment; filename=\"$ext.csv\"");
 	$cquery = query('SELECT * FROM card_ext, card  WHERE `card_ext`.`ext` = '.$e->id.' AND `card`.`id` = `card_ext`.`card` ORDER BY `card`.`name`', 'Card selection') ;
-	echo '"Rarity";"Name";"Color";"Cost";Converted;"Supertypes";"Types";"Subtypes"'."\n" ;
+	echo '"Rarity";"Name";"Color";"Cost";Converted;"Supertypes";"Types";"Subtypes";"Text"'."\n" ;
 	while ( $c = mysql_fetch_object($cquery) ) {
 		$j = json_decode($c->attrs) ;
 		echo '"'.$c->rarity.'";"'.$c->name.'";"'.$j->color.'";"'.$c->cost.'";'.$j->converted_cost.';"' ;
@@ -19,6 +19,7 @@ if ( $e = mysql_fetch_object($equery) ) {
 		echo '";"' ;
 		if ( property_exists($j, 'subtypes') )
 			echo implode(' ', $j->subtypes) ;
+		echo '";"' . $c->text;
 		echo '"'."\n" ;
 	}
 } else 
