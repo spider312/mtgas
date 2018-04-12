f809a9814eaff31a1125735f03831ecbca0526ae
spider312 <github@spiderou.net>
2018-04-12 20:24:57 +0200

    Better error management

--- json/cards.php
+++ json/cards.php
@@ -2,5 +2,16 @@
 include '../lib.php' ;
 include '../includes/db.php' ;
 include '../includes/card.php' ;
-die(json_encode(card_search($_GET, card_connect()))) ;
+$results = card_search($_GET, card_connect()) ;
+$json = json_encode($results) ;
+if ( $json === false ) {
+	foreach ($results->cards as $card) {
+		$json = json_encode($card) ;
+		if ( $json === false ) {
+			echo json_last_error_msg() ;
+			print_r($card) ;
+		}
+	}
+}
+die($json) ;
 ?>
