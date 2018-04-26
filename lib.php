beca8fabec9651b78d91a481c146f15f346bd6ed
spider312 <github@spiderou.net>
2018-04-26 18:35:28 +0200

    Use MTG[FR] as default discord server

--- lib.php
+++ lib.php
@@ -23,7 +23,7 @@ menu_add(__('menu.gui'), $url.'/doc/GUI.php', __('menu.gui.title'), true) ;
 menu_add('<img src='.theme_image("youtube.png").' alt="Youtube icon"> Tutorials', 'https://www.youtube.com/playlist?list=PL2cnRVtBdqkVJZNql_RBeUj6MNQJ2y0nS', 'Video tutorials', true) ;
 menu_add(__('menu.chat'), 'http://tchat.rs2i.net/?chan=mtg&amp;soft=qwebirc', __('menu.chat.title'), true) ;
 //menu_add(__('menu.forum'), 'http://forum.mogg.fr', __('menu.forum.title'), true) ;
-menu_add('<img src='.theme_image("discord.png").' alt="Discord icon"> Vocal', 'https://discord.gg/nnXznAW', 'Vocal and textual chat on Discord', true) ;
+menu_add('<img src='.theme_image("discord.png").' alt="Discord icon"> Vocal', 'https://discord.gg/V2UMH4q', 'Vocal and textual chat on Discord', true) ;
 menu_add('DevBlog', 'http://blog.mogg.fr/', 'Blog about new features', true) ;
 menu_add('Forum', 'http://forum.mogg.fr/', 'Bulletin board for discussions', true) ;
 
