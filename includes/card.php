18dbd4b6d843654a81a226d057e34dcf4e0a6e4b
spider312 <github@spiderou.net>
2019-04-19 15:17:40 +0200

    Manage all colors tokens creation

--- includes/card.php
+++ includes/card.php
@@ -854,7 +854,11 @@ function manage_text($name, $text, $target) {
 		$target->living_weapon = true ;
 	// Token creation
 	$colreg = implode('|', $colors) ;
-	if ( preg_match_all('/(?<number>\w+) (?<tapped>tapped )?((?<pow>\d*|X|\*+)\/(?<tou>\d*|X|\*+) )?(?<color>'.$colreg.') (and (?<color2>'.$colreg.') )?(?<types>[\w| ]+ creature) tokens?(?<attrs> with .*)?/', $text, $all_matches, PREG_SET_ORDER) ) {
+	if (
+		preg_match_all('/Create (?<number>\w+) (?<tapped>tapped )?(?<pow>\d*|X|\*+)\/(?<tou>\d*|X|\*+) (?<types>[\w| ]+ creature) tokens? that\'s all colors(?<attrs> with .*)?/', $text, $all_matches, PREG_SET_ORDER)
+		||
+		preg_match_all('/(?<number>\w+) (?<tapped>tapped )?((?<pow>\d*|X|\*+)\/(?<tou>\d*|X|\*+) )?(?<color>'.$colreg.') (and (?<color2>'.$colreg.') )?(?<types>[\w| ]+ creature) tokens?(?<attrs> with .*)?/', $text, $all_matches, PREG_SET_ORDER)
+	) {
 	// Godsire, Hazezon Tamar
 	//|| preg_match_all('/(?<number>\w+) (?<pow>\d*)\/(?<tou>\d*) (?<types>[\w| ]+ creature) tokens? that[\'s| are] (?<color>'.$colreg.'), (?<color2>'.$colreg.'), and (?<color3>'.$colreg.')/', $text, $all_matches, PREG_SET_ORDER)
 		foreach ( $all_matches as $matches ) {
@@ -877,11 +881,15 @@ function manage_text($name, $text, $target) {
 			// Other attrs
 			$token->attrs->pow = text2number($matches['pow'], 0) ; // 0 for image
 			$token->attrs->thou = text2number($matches['tou'], 0) ;
-			$token->attrs->color = array_search($matches['color'], $colors) . array_search($matches['color2'], $colors) ;
+			if ( array_key_exists('color', $matches) ) {
+				$token->attrs->color = array_search($matches['color'], $colors) . array_search($matches['color2'], $colors) ;
+			} else {
+				$token->attrs->color = 'WUBRG' ;
+			}
 			if ( $matches['tapped'] !== '' ) {
 				$token->attrs->tapped = true ;
 			}
-			if ( isset($matches['attrs']) ) {
+			if ( array_key_exists('attrs', $matches) ) {
 				apply_all_creat_attrs($token->attrs, $matches['attrs']) ;
 			}
 			$target->tokens[] = $token ;
