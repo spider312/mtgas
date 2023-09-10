2690f523501e2aadd1162bba9e0e0a2c155d43b5
spider312 <github@spiderou.net>
2023-09-10 21:06:28 +0200

    Don't import Extended-Art Frame as of the Jumpstart import ignore

--- admin/cards/import/importer/mv_dom.php
+++ admin/cards/import/importer/mv_dom.php
@@ -6,7 +6,7 @@ $mana_url = '/fr/graph/manas/big_png/' ;
 $rarity_url = 'graph/rarity/carte' ;
 $rarities = array(4 => 'M', 5 => 'S', 10 => 'R', 20 => 'U', 30 => 'C', 40 => 'L') ;
 //$imported_extratxt = array('Story Spotlight', 'Spotlight', 'Extended-Art Frame', 'Showcase Frame', 'Double Masters Prerelease Promo', 'Borderless', 'Buy-a-Box', 'Promo Pack') ;
-$not_imported_extratxt = array('Jumpstart pack') ;
+$not_imported_extratxt = array('Jumpstart pack', 'Extended-Art Frame') ;
 
 // Importer
 $import_url = $base_url.$base_path.'set_cards?setcode='.$ext_source.'&lang=eng' ;
