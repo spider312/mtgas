2139c618464b4baa2203b86e3597df26f3d750ae
spider312 <github@spiderou.net>
2025-11-11 14:08:44 +0100

    Rework + display message as log (pre)

--- admin/cards/compile.php
+++ admin/cards/compile.php
@@ -1,11 +1,17 @@
 <?php
 include_once 'lib.php' ;
 include 'import/HtmlDiff.php' ;
+
+// Apply ?
+$apply = param($_GET, 'apply', false) ;
+if ( $apply !== false ) {
+	$apply = true ;
+}
 ?>
 <html>
-<head>
-<link type="text/css" rel="stylesheet" href="../../themes/jay_kay/css/diff.css">
-<style>
+ <head>
+  <link type="text/css" rel="stylesheet" href="../../themes/jay_kay/css/diff.css">
+  <style>
 table {
 	width: 100%;
 }
@@ -13,20 +19,13 @@ td {
 	border: 1px solid black ;
 	white-space: pre;
 }
-</style>
-</head>
-<body>
-<table>
- <tr>
-  <th>Card</th>
-  <th>Diff</th>
- </tr>
+  </style>
+ </head>
+ <body>
+  <pre>
 <?php
-// Apply ?
-$apply = param($_GET, 'apply', false) ;
-if ( $apply !== false )
-	$apply = true ;
-// Loop
+// Create a list of changed attrs + Apply if needed
+$changed = [] ;
 $query = query('SELECT * FROM card ORDER BY `id` DESC') ; // Last added cards are managed first, in order not to wait before a die
 $nb = 0 ;
 while ( $arr = mysql_fetch_array($query) ) {
@@ -35,10 +34,8 @@ while ( $arr = mysql_fetch_array($query) ) {
 	$attrs = json_encode($attrs_obj) ;
 	if ( $arr['attrs'] != $attrs ) {
 		$diff = new HtmlDiff(jsonpp($arr['attrs']), jsonpp($attrs)) ;
-		$nb++ ;
-		echo '<tr>' ;
-		echo '<td><a href="card.php?id='.$arr['id'].'">'.$arr['name'].'</a></td>' ;
-		echo '<td>'.$diff->build().'</td>';
+		$arr['diff'] = $diff->build() ;
+		$changed[] = $arr ;
 		if ( $apply ) {
 			query("UPDATE
 				card
@@ -49,11 +46,24 @@ while ( $arr = mysql_fetch_array($query) ) {
 				`id` = '".$arr['id']."'
 			; ") ;
 		}
-		echo '</tr>' ;
 	}
 }
 ?>
-<caption><?php echo $nb ; ?> updates <a href="?apply=1">apply</a></caption>
-</table>
-</body>
+  </pre>
+  <table>
+   <tr>
+    <th>Card</th>
+    <th>Diff</th>
+   </tr>
+<?php
+foreach ( $changed as $arr ) {
+	echo '<tr>' ;
+	echo '<td><a href="card.php?id='.$arr['id'].'">'.$arr['name'].'</a></td>' ;
+	echo '<td>'.$arr['diff'].'</td>';
+	echo '</tr>' ;
+}
+?>
+   <caption><?php echo count($changed); ; ?> updates <a href="?apply=1">apply</a></caption>
+  </table>
+ </body>
 </html>
